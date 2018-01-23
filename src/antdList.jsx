f1c9c3ab2a4af7eed369cd3b33823dbe10166b50
yagamiram <sxk138130@utdallas.edu>
2018-01-23 11:07:35 -0800

    Added tooltip for icon at the footer of each event

--- src/antdList.jsx
+++ src/antdList.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { List, Icon } from 'antd';
+import { List, Icon, Tooltip } from 'antd';
 import 'antd/dist/antd.css';
 import moment from 'moment';
 
-const IconText = ({ type, text, event }) => (
+const IconText = ({ type, text, toolTipText, event }) => (
     <span>
-    <Icon type={type} style={{ marginRight: 8 }} onMouseOver={event}/>
-        {text}
-  </span>
+        <Tooltip placement={"top"} title={toolTipText}>
+            <Icon type={type} style={{ marginRight: 8 }} onMouseOver={event}/>
+            {text}
+        </Tooltip>
+    </span>
 );
 
 class AntdList extends React.Component {
@@ -46,7 +48,7 @@ class AntdList extends React.Component {
                 renderItem={item => (
                     <List.Item
                         key={item.title}
-                        actions={[<IconText type="clock-circle-o" text={moment(item.time).format("MMMM Do YYYY, h:mm:ss a")} event={() => {console.log("on click")}}/>, <IconText type="like-o" text={item.interest_count} event={() => {console.log("on click")}}/>, <IconText type="check-circle" text={item.attendingCount} event={() => {console.log("on click")}}/>]}
+                        actions={[<IconText type="clock-circle-o" text={moment(item.time).format("MMMM Do YYYY, h:mm:ss a")} toolTipText={"Time"} event={() => {console.log("on click")}}/>, <IconText type="like-o" text={item.interest_count} toolTipText={"Interested Count"} event={() => {console.log("on click")}}/>, <IconText type="check-circle" text={item.attendingCount} toolTipText={"Attending Count"} event={() => {console.log("on click")}}/>]}
                         extra={<img width={272} alt="logo" src={item.avatar} />}
                     >
                         <List.Item.Meta
